10aa2a870737aa250e103a3f3b337d87d6d22775
Aubree Abril <aubree.abril@gmail.com>
2017-12-14 03:08:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,31 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
+  const l = cart.length;
+  if (!1) {
+    return console.log("Your shopping cart is empty.");
+  }
+
+  let itemsAndPrices = [];
+
+  for (let i = 0; i < l; i++) {
+    let itemAndPrice = cart[i];
+    let item = Object.keys(itemAndPrice)[0];
+    let price = itemAndPrice[item];
+
+    itemsAndPrices.push(`${item} at \$${price}`);
+  }
+  switch(itemsAndPrices.length) {
+    case 1:
+      break;
+    case 2:
+      itemsAndPrices = itemsAndPrices.join(" and");
+      break;
+      default:
+        itemsAndPrices[l-1] = "and ".concat(itemsAndPrices[l-1]);
+        itemsAndPrices = itemsAndPrices.join(", ");
+  }
+  console.log(`In your cart, you have ${itemsAndPrices}.`);
 }
 
 function total() {
